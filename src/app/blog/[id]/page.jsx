28d49ffaa3cf417ab8e33ec66c72b6dbdde15aae
avocado0908 <avocado0908@gmail.com>
2026-01-27 18:27:45 +1300

    Add blog page UI

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
 import styles from './page.module.css'
-  
+import Link from 'next/link'
+import Image from 'next/image' 
 
 const BlogPost = () => {
   return (
-    <div className={styles.container}>BlogPost</div>
+     <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>title</h1>
+          <p className={styles.desc}>
+            Description
+          </p>
+          <div className={styles.author}>
+            <Image
+              src={'https://images.pexels.com/photos/4065136/pexels-photo-4065136.jpeg?auto=compress&cs=tinysrgb&w=600q'}
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>username</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src={'https://images.pexels.com/photos/4065136/pexels-photo-4065136.jpeg?auto=compress&cs=tinysrgb&w=600q'}
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+         content
+        </p>
+      </div>
+    </div>
   )
 }
 
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import styles from './page.module.css'
-  
+import Image from 'next/image'
+import Link from 'next/link'  
+
 const Blog = () => {
   return (
-    <div className={styles.container}>Blog</div>
+    <div className={styles.mainContainer}>
+      
+        <Link href='/blog/testId' className={styles.container} >
+          <div className={styles.imageContainer}>
+            <Image
+              src="https://images.pexels.com/photos/4065133/pexels-photo-4065133.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>Test</h1>
+            <p className={styles.desc}>Description</p>
+          </div>
+        </Link>
+
+        <Link href='/blog/testId' className={styles.container} >
+          <div className={styles.imageContainer}>
+            <Image
+              src="https://images.pexels.com/photos/4065133/pexels-photo-4065133.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>Test</h1>
+            <p className={styles.desc}>Description</p>
+          </div>
+        </Link>
+      
+    </div>
   )
 }
 
