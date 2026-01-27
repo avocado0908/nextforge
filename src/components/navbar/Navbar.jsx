53454e07e81f28d2c015eda9bf82046a5e13909b
avocado0908 <avocado0908@gmail.com>
2026-01-27 15:41:54 +1300

    Apply global styles and CSS modules

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client"
 import Link from 'next/link'
 import React from 'react'
+import styles from './navbar.module.css'
 
 const links = [
     { id: 1, title: 'Home', url: '/' },
@@ -13,13 +14,13 @@ const links = [
 
 const Navbar = () => {
   return (
-    <div>
-        <Link href="/">Taytech</Link>
-        <div>
+    <div className={styles.container}>
+        <Link href="/" className={styles.logo}>Taytech</Link>
+        <div className={styles.links}>
             {links.map(link => (
-                <Link key={link.id} href={link.url}>{link.title}</Link>
+                <Link key={link.id} href={link.url} className={styles.link}>{link.title}</Link>
             ))}
-            <button onClick={() => {console.log("Logged out");}}>Logout</button>
+            <button className={styles.logout} onClick={() => {console.log("Logged out");}}>Logout</button>
             
         </div>
     </div>
