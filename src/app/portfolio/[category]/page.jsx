12da3cfe559c8dfe9b17de8d7c96de81e31362da
avocado0908 <avocado0908@gmail.com>
2026-01-28 18:44:34 +1300

    Fetch data from local JSON file

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,33 +1,39 @@
 import React from 'react'
 import styles from './page.module.css'
 import Button from '@/components/Button/Button';
+import {items} from "./data.js"
+import {notFound} from 'next/navigation';
+
+const getData = async (cat) => {
+  const data = items[cat]
+
+  if (data) {
+    return data;
+  }
+
+  return notFound();
+}
 
 const Category = async ({ params }) => {
   const resolvedParams = await params;
-  console.log(resolvedParams);
+    const data = await getData(resolvedParams.category);
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{resolvedParams.category}</h1>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Description</p>
-          <Button text="See More" url='#'/>
+
+      {data.map((item) => ( 
+        
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button text="See More" url='#'/>
+          </div>
+          <div className={styles.imgContainer}>
+            <img src={item.image} alt="" className={styles.img} />
+          </div>
         </div>
-        <div className={styles.imgContainer}>
-          <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" className={styles.img} />
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Description</p>
-          <Button text="See More" url='#'/>
-        </div>
-        <div className={styles.imgContainer}>
-          <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" className={styles.img} />
-        </div>
-      </div>
+      ))}
     </div>
   )
 }
